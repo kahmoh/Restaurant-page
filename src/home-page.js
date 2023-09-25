f5f2b1246077908c49bd8fb0a038ef685238708b
kahmoh <joshmcfadden17@gmail.com>
2023-09-25 02:01:31 +0100

    Improve home page rendering logic -move renderHeader function to index.js to allow other modules to use the function -rewrite home page rendering functionality into an object for readability -add renderChickenImage to homePage object

--- src/home-page.js
+++ src/home-page.js
@@ -1,47 +1,18 @@
-const content = document.querySelector('#content')
+import ChickenImage from '../src/gallery_nandos_new_cutlery_2.jpg'
+import {renderHeader,content} from "./index";
 
-const renderHomePage = function () {
-    renderHeader();
-};
 
-const renderHeader = function () {
-    const headerContainer = document.createElement('div');
-    headerContainer.appendChild(renderTitle());
-    headerContainer.appendChild(renderLinks(createLinks()));
-    content.appendChild(headerContainer);
-};
 
-const renderTitle = function () {
-    const title = document.createElement('h1')
-    title.textContent = 'home'
-    return title;
+const homePage = {
+    renderHomePage: function () {
+        content.appendChild(renderHeader());
+        content.appendChild(this.renderChickenImage())
+    },
+    renderChickenImage: function () {
+        const image = new Image()
+        image.src = ChickenImage;
+        return image;
+    },
 }
 
-const createLink = function (content) {
-    const link = document.createElement('li')
-    link.textContent = content;
-    return link;
-}
-
-const createLinks = function () {
-    const linksArray = [];
-    const linkContentArray = ['home','menu','contact'];
-    for (let i = 0; i < linkContentArray; i++) {
-        linksArray.push(createLink(linkContentArray[i]));
-        console.log('link created')
-    }
-    return linksArray;
-}
-
-createLinks()
-
-const renderLinks = function (array) {
-    const linkContainer = document.createElement('div');
-    array.forEach(element => {
-        linkContainer.appendChild(element);
-    });
-    console.log(array)
-    return linkContainer;
-}
-
-export {renderHomePage}
+export {homePage,renderHeader}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import './style.css';
 
-import {renderHomePage} from "./home-page";
+import {homePage} from "./home-page";
 
-renderHomePage()
+const content = document.querySelector('#content')
+
+const renderHeader = function () {
+    const headerContainer = document.createElement('div');
+    headerContainer.appendChild(renderTitle());
+    headerContainer.appendChild(links.renderLinks());
+    headerContainer.style.display = 'flex'
+    return headerContainer
+};
+
+const renderTitle = function () {
+    const title = document.createElement('h1')
+    title.textContent = 'home'
+    return title;
+}
+
+const links = {
+    linksContentArray: ['about', 'contact'],
+    renderLink: function (content) {
+        const link = document.createElement('a')
+        link.href = '#'
+        link.textContent = content;
+        return link;
+    },
+    renderLinks: function () {
+        const linkContainer = document.createElement('div');
+        linkContainer.style.display = 'flex'
+        linkContainer.style.flexDirection = 'column'
+        this.linksContentArray.forEach(element => {
+            const currentLink  = this.renderLink(element);
+            linkContainer.appendChild(currentLink);
+        });
+        return linkContainer;
+    },
+}
+
+export {renderHeader,content}
+
+homePage.renderHomePage()
